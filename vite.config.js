3f4bc03b0417dcaaca57e26d22da77e0bb5090e7
NabeelAttar <nabeelattar2411@gmail.com>
2025-09-08 02:02:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 10000,
+    port: process.env.PORT || 10000
   },
-}
+  preview: {
+    allowedHosts: ['blogged-03bk.onrender.com']
+  }
+});
+
 
